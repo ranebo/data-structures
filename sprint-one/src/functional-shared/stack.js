1ce00493b9ea77165a434c97dbd4a24ce184e58d
ranebo <ranegri3@gmail.com>
2016-03-02 15:40:52 -0800

    First attempt passing - using extend to add properties to new stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,73 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.items = 0;
+  extend(newStack, Stack.stackMethods);
+  return newStack;
 };
 
-var stackMethods = {};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+Stack.stackMethods = {};
+
+Stack.stackMethods.push = function(){
+    this.items = value;
+};
+
+Stack.stackMethods.pop = function(){
+
+};
+
+Stack.stackMethods.size = function(){
+  return 0;
+
+};
 
 
+
+
+
+
+
+// var giraffeMaker = function(name, height) {
+//   var newGiraffe = {};
+//   newGiraffe.name = name;
+//   newGiraffe.height = height;
+//   newGiraffe.hunger = 10;
+//   extend(newGiraffe, giraffeMaker.giraffeMethods);
+
+//   return newGiraffe;
+// };
+
+// var extend = function(to, from) {
+//   for (var key in from) {
+//     to[key] = from[key];
+//   }
+// };
+
+// giraffeMaker.giraffeMethods = {};
+
+
+// giraffeMaker.giraffeMethods.isTallEnough = function(treeHeight) {
+//     return this.height > treeHeight;
+//   };
+
+// giraffeMaker.giraffeMethods.isHungry = function() {
+//   return this.hunger > 0;
+// };
+
+// giraffeMaker.giraffeMethods.say = function(option) {
+//   var sentences = {
+//     'greet': 'Hello, my name is ' + this.name + ', it is nice to meet you.',
+//     'notHungry': this.name + ' is not hungry.',
+//     'notTallEnough': this.name + ' is too short to reach the trees.',
+//     'ate': 'That was delicious!'
+//   };
+
+//   return console.log(sentences[option]);
+// };
