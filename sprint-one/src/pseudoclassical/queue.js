03f1b26f6330482a9943da5d932c7cb1a2b16b8f
ranebo <ranegri3@gmail.com>
2016-03-02 16:36:47 -0800

    Pass all tests in pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.item = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.item] = value;
+  this.item++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.item > 0) {
+    this.item--;
+  }
+  var popped = this.storage[0];
+  delete this.storage[0];
+
+  for (var key in this.storage) {
+    var count = 0;
+    this.storage[count] = this.storage[key];
+    count++;
+  }
+  return popped;
+};
+
+Queue.prototype.size = function() {
+  return this.item;
+};
+
+var newQueue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.item = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.item] = value;
+  this.item++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.item !== 0) {
+    this.item--;
+  }
+  var popped = this.storage[this.item];
+  delete this.storage[this.item];
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  return this.item;
+};
+
+var newStack = new Stack();
 
