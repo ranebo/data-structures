34680983e93a599739a3d034fe438a99b8a5ecc6
ranebo <ranegri3@gmail.com>
2016-03-03 10:59:18 -0800

    Pass all linked list tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,37 @@
 var LinkedList = function() {
   var list = {};
+  list.length = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    list.length++;
   };
 
   list.removeHead = function() {
+    var remove = list.head.value;
+    list.head = list.head.next;
+    list.length--;
+    return remove;
   };
 
   list.contains = function(target) {
+    var check = list.head;
+    for (var i = 0; i < list.length; i++) {
+      if (check.value === target) {
+        return true;
+      }
+      check = check.next;
+    }
+    return false;
   };
 
   return list;
@@ -25,5 +47,9 @@ var Node = function(value) {
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ *  Complexity: What is the time complexity of the above functions?
+ *  list.addToTail is Constant
+ *  list.removeHead is Constant
+ *  list.contains is Linear Time
  */
+
