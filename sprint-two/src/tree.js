f6ff22cbde1cc58905e55edefba8fb67beba2c07
ranebo <ranegri3@gmail.com>
2016-03-03 13:38:29 -0800

    Pass all tests in tree SpecRunner

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,41 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  extend(newTree, treeMethods);
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-};
 
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Tree - CONSTANT
+ extend - LINEAR
+ treeMethods.addChild - CONSTANT
+ treemethods.contains - LINEAR
  */
