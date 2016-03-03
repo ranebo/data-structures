5d62f793261389a0e6a65a5fead4350873c45a9b
ranebo <ranegri3@gmail.com>
2016-03-03 15:20:22 -0800

    Not add in values that are already in the set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,6 +7,11 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return;
+    }
+  }
   this._storage.push(item);
 };
 
@@ -30,7 +35,7 @@ setPrototype.remove = function(item) {
 /*
  * Complexity: What is the time complexity of the above functions?
  Set - CONSTANT
- setPrototype.add - CONSTANT
+ setPrototype.add - LINEAR
  setPrototype.contains - LINEAR
  setPrototype.remove - LINEAR
  */
