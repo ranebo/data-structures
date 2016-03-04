b839465c63b720c8f0867bc0532087351db57044
ranebo <ranegri3@gmail.com>
2016-03-03 18:47:36 -0800

    Pass all tests in hashTable in SpecRunner

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,4 @@
-
+ 
 
 var HashTable = function() {
   this._limit = 8;
@@ -7,16 +7,37 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var newTuple = [k, v];
+  if (this._storage[index]) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+      } else {
+        this._storage[index].push(newTuple);
+      }
+    }
+  } else {
+    this._storage[index] = [newTuple];
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
-
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      delete this._storage[index][i][0];
+    }
+  }
+};  
 
 
 /*
