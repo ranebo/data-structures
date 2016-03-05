37262ea6b07c6c7b78b64514b230854597d4f892
ranebo <ranegri3@gmail.com>
2016-03-04 20:11:01 -0800

    Add unfinished hashTable resize method in comments

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,11 +1,12 @@
- 
 
 var HashTable = function() {
+  this._items = 0;
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
+  this._items++;
   var index = getIndexBelowMaxForKey(k, this._limit);
   var newTuple = [k, v];
   if (this._storage[index]) {
@@ -19,6 +20,10 @@ HashTable.prototype.insert = function(k, v) {
   } else {
     this._storage[index] = [newTuple];
   }
+  if (this._items / this._limit > 0.75 ) {
+    this._limit = this._limit * 2;
+    this.resize(this._limit);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -31,14 +36,39 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  this._items && this._items--;
   var index = getIndexBelowMaxForKey(k, this._limit);
   for (var i = 0; i < this._storage[index].length; i++) {
     if (this._storage[index][i][0] === k) {
       delete this._storage[index][i][0];
     }
   }
+  if (this._items / this._limit < 0.25 ) {
+    this._limit = this._limit / 2;
+    this.resize(this._limit);
+  }
 };  
 
+// Hashtable.prototype.resize = function(newLimit) {
+
+  
+//   var newStorage = LimitedArray(newLimit);
+//   for (var i = 0; i < this._storage.length; i++) {
+//     for (var j = 0; j < this._storage[i].length; j++) {
+//       var index = getIndexBelowMaxForKey(this._storage[i][j][0], newLimit);
+//         if (newStorage) {
+
+//         }
+//       newStorage.insert(this._storage[i][j][0], this._storage[i][j][1]);
+
+//     }
+//   }
+//   //loop through buckets
+//     //loop through tuples
+//       //each tuple call this.insert(tubple[0], tuple[1])
+
+//   this._storage = newStorage;
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
